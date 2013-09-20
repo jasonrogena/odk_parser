b0c5517fd0fcb23826e1744d7d7d4f29c61440d1
Jason Rogena <jasonrogena@gmail.com>
2013-09-20 14:13:53 +0300

    Initial commit, successfully parsing json objects/arrays to excel documents

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html public "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="Description" content="A tool custom made for ILRI (International Livestock Research Institute) to convert json files recieved from ODK Aggregate to Excel files" />
+      <meta name="robots" content="index,follow" />
+      <title>ODK Parser</title>
+   </head>
+   <body>
+      <?php
+      // put your code here
+      ?>
+   </body>
+</html>
---
+++ modules/Parse.php
@@ -0,0 +1,225 @@
+<?php
+error_reporting(E_ALL);
+
+class Parser {
+   private $TAG = "Parse.php";
+   private $ROOT = "../";
+   private $logHandler;
+   private $settings;
+   private $phpExcel;
+   private $jsonObject;
+   private $sheetIndexes;
+   private $allColumnNames;
+   private $nextRowName;
+   
+   public function __construct() {
+      //load settings
+      $this->loadSettings();
+      
+      //include modules
+      include_once $this->ROOT.'modules/Log.php';
+      $this->logHandler = new LogHandler();
+      
+      include_once $this->ROOT.'modules/PHPExcel.php';
+      $this->phpExcel = new PHPExcel();
+      $this->setExcelMetaData();
+      
+      //init other vars
+      $this->sheetIndexes = array();
+      $this->allColumnNames = array();
+      $this->nextRowName = array();
+      
+      //parse json String
+      $this->parseJson();
+      
+      //process all responses
+      $mainSheetKey = "main_sheet";
+      
+      foreach($this->jsonObject as $currentJsonObject) {
+         $this->createSheetRow($currentJsonObject, $mainSheetKey);
+      }
+      
+      //save the excel object
+      $objWriter = new PHPExcel_Writer_Excel2007($this->phpExcel);
+      $objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+   }
+   
+   private function loadSettings() {
+      $settingsDir = $this->ROOT."config/";
+      if(file_exists($settingsDir."main.ini")) {
+         $this->settings = parse_ini_file($settingsDir."main.ini");
+      }
+   }
+   
+   private function setExcelMetaData() {
+      //TODO: get metadata from POST
+      $this->phpExcel->getProperties()->setCreator("Jason Rogena");
+      $this->phpExcel->getProperties()->setLastModifiedBy("Jason Rogena");
+      $this->phpExcel->getProperties()->setTitle("Test");
+      $this->phpExcel->getProperties()->setSubject("Testing");
+      $this->phpExcel->getProperties()->setDescription("Test by Jason Rogena");
+   }
+   
+   private function getColumnName($parentKey, $key){//a maximum of 676 (26*26) columns
+      $columnNames = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+      $indexOfKey = array_search($key, $this->allColumnNames[$parentKey]);
+      $x = intval($indexOfKey/26) -1;
+      $y = fmod($indexOfKey, 26);
+      $columnName = "";
+      if($x>=0){
+         $columnName = $columnNames[$x];
+      }
+      if($y<26){
+         $columnName = $columnName.$columnNames[$y];
+         echo 'returned from getcolumn name '.$columnName.'<br/>';
+         return $columnName;
+      }
+      
+   }
+   
+   private function createSheetRow($jsonObject, $parentKey, $parentCellName = NULL) {
+      //check if sheet for parent key exists
+      $sheetArrayKeys = array_keys($this->sheetIndexes);
+      if(!in_array($parentKey, $sheetArrayKeys)) {
+         echo 'sheet for '.$parentKey.' does not exist<br/>';
+         //create sheet for parent key
+         echo 'size of sheet indexes before '.sizeof($this->sheetIndexes)."<br/>";
+         $this->sheetIndexes[$parentKey] = sizeof($this->sheetIndexes);
+         echo 'size of sheet indexes now '.sizeof($this->sheetIndexes)."<br/>";
+         $this->nextRowName[$parentKey] = 1;
+         $this->allColumnNames[$parentKey] = array();
+         
+         if(sizeof($this->sheetIndexes)>1){
+            $this->phpExcel->createSheet();
+            echo 'this is not the first sheet, therefore calling createSheet<br/>';
+         }
+         $this->phpExcel->setActiveSheetIndex($this->sheetIndexes[$parentKey]);
+         echo 'set active sheet index to '.$this->sheetIndexes[$parentKey]."<br/>";
+         $this->phpExcel->getActiveSheet()->setTitle($parentKey);
+      }
+      else {
+         //set active sheet to that which corresponds to parent key
+         $this->phpExcel->setActiveSheetIndex($this->sheetIndexes[$parentKey]);
+         echo 'sheet for '.$parentKey.' already exists<br/>';
+      }
+      
+      //split keys and values in jsonObject
+      echo 'splitting keys and values in jsonObject<br/>';
+      $keys = array_keys($jsonObject);
+      $values = array();
+      $index = 0;
+      foreach($jsonObject as $value) {
+         $values[$index] = $value;
+         $index++;
+      }
+      echo 'size of values is '.sizeof($values)."<br/>";
+      
+      //get next row name for parent key
+      $rowName = $this->nextRowName[$parentKey];
+      echo 'row name is '.$rowName.'<br/>';
+      
+      //set name of parent cell as first cell in row if is set
+      if ($parentCellName != NULL) {
+         if (!in_array($this->allColumnNames[$parentKey], "Parent_Cell")) {
+            echo 'pushing Parent_Cell to allColumnNames array for ' . $parentKey . '<br/>';
+            array_push($this->allColumnNames[$parentKey], "Parent_Cell");
+            //$this->allColumnNames[$parentKey][sizeof($this->allColumnNames[$parentKey])]="Parent_Cell";
+         }
+         $columnName = $this->getColumnName($parentKey, "Parent_Cell");
+         if ($columnName != FALSE) {
+            $cellName = $columnName . $rowName;
+            $this->phpExcel->getActiveSheet()->setCellValue($cellName, $parentCellName);
+            $this->phpExcel->getActiveSheet()->getColumnDimension($columnName)->setAutoSize(true);
+            //$this->phpExcel->getActiveSheet()->getStyle($cellName)->getAlignment()->setWrapText(true);
+         } else {
+            echo 'column name for Parent_Cell not found<br/>';
+            print_r($this->allColumnNames[$parentCellName]);
+         }
+      }
+      
+      //add all keys and respective values to row
+      if(sizeof($keys) == sizeof($values) && sizeof($value) > 0) {
+      
+         echo 'adding columns from here<br/>';
+         for($index = 0; $index < sizeof($keys); $index++) {
+            //add key to allColumns array
+            $columnExisted = TRUE;
+            if(!in_array($this->allColumnNames[$parentKey], $keys[$index])) {
+               $columnExisted = FALSE;
+               echo 'pushing '.$keys[$index].' to allColumnNames array for '.$parentKey.'<br/>';
+               //array_push($this->allColumnNames[$parentKey], $keys[$index]);
+               $this->allColumnNames[$parentKey][sizeof($this->allColumnNames[$parentKey])]=$keys[$index];
+               
+            }
+            
+            $columnName = $this->getColumnName($parentKey, $keys[$index]);
+            if($columnExisted == FALSE) {
+               $this->phpExcel->getActiveSheet()->getColumnDimension($columnName)->setAutoSize(true);
+            }
+            
+            if ($columnName != FALSE) {
+               $cellName = $columnName . $rowName;
+               $this->phpExcel->setActiveSheetIndex($this->sheetIndexes[$parentKey]);
+
+               if (!is_array($values[$index])) {
+                  echo 'value for '.$keys[$index].' is normal value<br/>';
+                  echo 'value of '.$keys[$index].' is '.$values[$index].'<br/>';
+                  if($values[$index]== "") {
+                     $values[$index] = "NULL";
+                  }
+                  $this->phpExcel->getActiveSheet()->setCellValue($cellName, $values[$index]);
+               } 
+               else {//if values is an array
+                  if (sizeof($values[$index] > 0)) {
+                     echo 'value of '.$keys[$index].' is an array<br/>';
+                     $this->phpExcel->getActiveSheet()->setCellValue($cellName, "Check " . $keys[$index] . " sheet");
+                     foreach ($values[$index] as $childJsonObject) {
+                        $this->createSheetRow($childJsonObject, $keys[$index], $parentKey." (".$cellName.")");
+                     }
+                  }
+                  else {
+                     echo 'value of '.$keys[$index].' is an array but is empty<br/>';
+                     $this->phpExcel->getActiveSheet()->setCellValue($cellName, "");
+                  }
+               }
+               //$this->phpExcel->getActiveSheet()->getStyle($cellName)->getAlignment()->setWrapText(true);
+            }
+            else {
+               echo 'column name for '.$keys[$index].' not found<br/>';
+               print_r($this->allColumnNames[$parentKey]);
+            }
+            
+         }
+      }
+      else {//probably means the jsonObject is just a string
+         echo 'adding row with only one column since json object is just string<br/>';
+         $columnExisted = TRUE;
+         if(!in_array($this->allColumnNames[$parentKey], "values")) {
+            $columnExisted = FALSE;
+            echo 'pushing values to allColumnNames array for ' . $parentKey . '<br/>';
+            //array_push($this->allColumnNames[$parentKey], $keys[$index]);
+            $this->allColumnNames[$parentKey][sizeof($this->allColumnNames[$parentKey])] = 'values';
+         }
+
+         $columnName = $this->getColumnName($parentKey, 'values');
+         if ($columnExisted == FALSE) {
+            $this->phpExcel->getActiveSheet()->getColumnDimension($columnName)->setAutoSize(true);
+         }
+         
+         if ($columnName != FALSE) {
+            $cellName = $columnName . $rowName;
+            $this->phpExcel->setActiveSheetIndex($this->sheetIndexes[$parentKey]);
+            $this->phpExcel->getActiveSheet()->setCellValue($cellName, $jsonObject);
+         }
+      }
+      $this->nextRowName[$parentKey]++;
+   }
+   
+   private function parseJson() {
+      $jsonString = '[{"start_time":"2013-09-18T17:15:12.000+03","DeviceID":"356262054210980","user_name":"collector","ltdc_id":"collector","qr":"http://en.m.wikipedia.org","site":null,"cluster":null,"hh_id":null,"hh_head":null,"info_prov":"Thomas","cycle_no":12,"gps:Latitude":-1.2692566667,"gps:Longitude":36.7220366667,"gps:Altitude":1889.8000488281,"gps:Accuracy":4.4000000000,"ee":[{"id":"Test","aq_disp":"acq","dt":"2013-07-18","count":2,"md":"aq_pur","re":"aqr_imp","pr":25,"spr":null,"md":null,"re":null,"pr":null,"bpr":null,"dt_c":null,"dt_other":null,"hm_count":0,"hm":"yes"},{"id":"That","aq_disp":"acq","dt":"2013-06-18","count":6,"md":"aq_gft","re":"aqr_imp","pr":47000,"spr":null,"md":null,"re":null,"pr":null,"bpr":null,"dt_c":null,"dt_other":null,"hm_count":null,"hm":"no"}],"dl":[{"c_id":"Btr","c_dt":"2013-09-18","c_tc":"tc_pm","c_ec":"ec_hp","c_bd":"2013-08-18","c_nm":null,"c_br":["bn_jr","bn_ex.ot","bn_an"],"c_sx":"sex_m","c_wc":"mw_uncon","c_fm":"mf_bf","c_st":"cs_al","c_iu":"iu_rh","c_def":"def_bl","c_dp":null,"dt":null,"re":null,"s_nm":"Gem","s_bd":"2013-09-18","s_br":["bn_ay","bn_sh","bn_ng"],"d_nm":"Him","d_bd":"2012-08-18","d_br":["bn_ay","bn_sh","bn_ex.ot","bn_ng"]}],"bs":[{"id":"Hek","st":"ail","st_r":["rsc_cl"],"ex_dt":"2013-05-18","bn":"That","si":"Red","ss":"ss_av","bo":"Neighbor","cs":20000,"cb":"15000","is_sc":"yes","sc_r":"scr_uet"}],"hr":[{"id":"Join","t_dt":"2013-08-18","dis":"dc_ms","dr_c":11000,"pr_c":12000,"sp":"sp_cahw","tt":"tt_ht","c_st":"psc_rec"}],"ph":[{"id":"Bus","ac":"ac_vc","e_dt":"2013-08-18","c_st":"psc_dd","dr_c":12000,"pr_c":15000,"sp":"sp_wpa","cm":"Something"}],"ms":[{"by":"Buttress","wk":12,"sm":36,"pr_l":21,"is_bc":"yes","bc_r":"rbc_pp"}]},{"start_time":"2013-09-18T17:15:12.000+03","DeviceID":"356262054210980","user_name":"collector","ltdc_id":"collector","qr":"http://en.m.wikipedia.org","site":null,"cluster":null,"hh_id":null,"hh_head":null,"info_prov":"Thomas","cycle_no":12,"gps:Latitude":-1.2692566667,"gps:Longitude":36.7220366667,"gps:Altitude":1889.8000488281,"gps:Accuracy":4.4000000000,"ee":[{"id":"Test","aq_disp":"acq","dt":"2013-07-18","count":2,"md":"aq_pur","re":"aqr_imp","pr":25,"spr":null,"md":null,"re":null,"pr":null,"bpr":null,"dt_c":null,"dt_other":null,"hm_count":0,"hm":"yes"},{"id":"That","aq_disp":"acq","dt":"2013-06-18","count":6,"md":"aq_gft","re":"aqr_imp","pr":47000,"spr":null,"md":null,"re":null,"pr":null,"bpr":null,"dt_c":null,"dt_other":null,"hm_count":null,"hm":"no"}],"dl":[{"c_id":"Btr","c_dt":"2013-09-18","c_tc":"tc_pm","c_ec":"ec_hp","c_bd":"2013-08-18","c_nm":null,"c_br":["bn_jr","bn_ex.ot","bn_an"],"c_sx":"sex_m","c_wc":"mw_uncon","c_fm":"mf_bf","c_st":"cs_al","c_iu":"iu_rh","c_def":"def_bl","c_dp":null,"dt":null,"re":null,"s_nm":"Gem","s_bd":"2013-09-18","s_br":["bn_ay","bn_sh","bn_ng"],"d_nm":"Him","d_bd":"2012-08-18","d_br":["bn_ay","bn_sh","bn_ex.ot","bn_ng"]}],"bs":[{"id":"Hek","st":"ail","st_r":["rsc_cl"],"ex_dt":"2013-05-18","bn":"That","si":"Red","ss":"ss_av","bo":"Neighbor","cs":20000,"cb":"15000","is_sc":"yes","sc_r":"scr_uet"}],"hr":[{"id":"Join","t_dt":"2013-08-18","dis":"dc_ms","dr_c":11000,"pr_c":12000,"sp":"sp_cahw","tt":"tt_ht","c_st":"psc_rec"}],"ph":[{"id":"Bus","ac":"ac_vc","e_dt":"2013-08-18","c_st":"psc_dd","dr_c":12000,"pr_c":15000,"sp":"sp_wpa","cm":"Something"}],"ms":[{"by":"Buttress","wk":12,"sm":36,"pr_l":21,"is_bc":"yes","bc_r":"rbc_pp"}]}]'; //TODO: get jsonString from post
+      $this->jsonObject = json_decode($jsonString, TRUE);
+   }
+}
+
+$obj = new Parser();
+?>
